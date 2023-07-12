8d5f183fdb65423e8bd7da3ffe544e07e115dab9
quanghm <quanghm27@gmail.com>
2023-07-12 16:46:42 +0700

    new rule

--- src/components/TimelineList/TimelineList.tsx
+++ src/components/TimelineList/TimelineList.tsx
@@ -1,43 +1,45 @@
 import styles from "./TimelineList.module.css";
 import { storage } from "../../fake.ts";
-import { useState,MouseEventHandler,useRef } from "react";
+import { useState, MouseEventHandler, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 type EventItem = {
-  title: string,
-  content: string,
-  offsetY: number,
-  isCollapse: boolean
+  title: string;
+  content: string;
+  offsetY: number;
+  isCollapse: boolean;
 };
 
 function TimelineList() {
-  const listTmp:EventItem[] = []
+  const listTmp: EventItem[] = [];
   const [listEventItem, setListEventItem] = useState(listTmp);
-
-
-  const timeLineRef = useRef(null);
-  const[date,setDate]=useState<number | undefined>();
+  const timeLineRef = useRef<HTMLDivElement>(null);
+  const [date, setDate] = useState<number | undefined>();
   const [lists, setLists] = useState(storage);
+  const [heightOfTimeLine, setHeightOfTimeLine] = useState(1000);
+
   const handleDelete = (id: string) => {
     const newArr = lists.filter((item) => item.id !== id);
     setLists([...newArr]);
   };
 
-  const handleChooseTimeline: MouseEventHandler<HTMLDivElement> = (e) =>{
-    const heightOfTimeLine = timeLineRef.current.getBoundingClientRect().height;
-    console.log("heightOfTimeLine:",heightOfTimeLine);
-    
-    const logPercent = Math.floor((e.pageY-170)/heightOfTimeLine*100);
-    console.log("value: ",`${logPercent} %`) ;
-    const day = Math.floor(1095*logPercent/100);
+  const handleChooseTimeline: MouseEventHandler<HTMLDivElement> = (e) => {
+    // const heightOfTimeLine:number | undefined = timeLineRef.current.getBoundingClientRect().height;
+    // console.log(heightOfTimeLine);
+    // setHeightOfTimeLine(heightOfTimeLine);
+    const heightOfTimeLine = 1000;
+    const logPercent = Math.floor(((e.pageY - 170) / heightOfTimeLine) * 100);
+    console.log("value: ", `${logPercent} %`);
+    const day = Math.floor((1095 * logPercent) / 100);
     setDate(day);
-    console.log("Day:",day);
+    console.log("Day:", day);
     setLists((prevLists) => [
       ...prevLists,
       {
         id: uuidv4(),
         title: "..aaaa.",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque quidem esse? Incidunt, odit beatae?",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque quidem esse? Incidunt, odit beatae?",
         createAt: "...",
       },
     ]);
@@ -48,12 +50,11 @@ function TimelineList() {
       <p>id:{list.id}</p>
       <h3 className={styles.title}>{list.title}</h3>
       <p>{list.content}</p>
-      <a >Edit &gt;</a>
+      <a>Edit &gt;</a>
       <a onClick={() => handleDelete(list.id)} className={styles["delete-btn"]}>
         Delete
       </a>
-      <span  className={styles.circle}>
-      </span>
+      <span className={styles.circle}></span>
       <span className={styles.line}></span>
       <span className={styles.date}>{list.createAt}</span>
     </li>
@@ -65,7 +66,12 @@ function TimelineList() {
         <h3 className={styles.heading}>Timeline</h3>
         <div className={styles.container}>
           <ul>{listItems}</ul>
-          <div ref={timeLineRef} onClick={ handleChooseTimeline} className={styles.timeline}>
+          <div
+            style={{ height: `${heightOfTimeLine}px` }}
+            ref={timeLineRef}
+            onClick={handleChooseTimeline}
+            className={styles.timeline}
+          >
             <div className={styles.circleBottom}></div>
             <div>{date}</div>
           </div>

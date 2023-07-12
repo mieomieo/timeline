f0f95f7e04a706781061f09921ba5d71dc26a259
quanghm <quanghm27@gmail.com>
2023-07-12 16:01:17 +0700

    new rule

--- src/components/TimelineList/TimelineList.tsx
+++ src/components/TimelineList/TimelineList.tsx
@@ -1,63 +1,63 @@
 import styles from "./TimelineList.module.css";
 import { storage } from "../../fake.ts";
-import { useState } from "react";
+import { useState,MouseEventHandler,useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+type EventItem = {
+  title: string,
+  content: string,
+  offsetY: number,
+  isCollapse: boolean
+};
+
 function TimelineList() {
-  const handleAddBetween = (id:string) =>{
-    // const newArr = [...l]
-    const addNew = {
-      id: uuidv4(),
-      title: "...",
-      content: "...",
-      createAt: "...",
-    };
-    const index:number = lists.findIndex(item=> item.id === id  )
-    console.log(index);
-    const newArr = [...lists];
-    newArr.splice(index,0,addNew)
-    console.log(newArr);
-    setLists([...newArr]);
-    
-  };
-  const handleEdit = (id: string) =>{
-  
-  };
+  const listTmp:EventItem[] = []
+  const [listEventItem, setListEventItem] = useState(listTmp);
+
+
+  const timeLineRef = useRef(null);
+  const[date,setDate]=useState<number | undefined>();
+  const [lists, setLists] = useState(storage);
   const handleDelete = (id: string) => {
     const newArr = lists.filter((item) => item.id !== id);
     setLists([...newArr]);
   };
-  const handleAdd = () => {
+
+  const handleChooseTimeline: MouseEventHandler<HTMLDivElement> = (e) =>{
+    const heightOfTimeLine = timeLineRef.current.getBoundingClientRect().height;
+    console.log("heightOfTimeLine:",heightOfTimeLine);
+    
+    const logPercent = Math.floor((e.pageY-170)/heightOfTimeLine*100);
+    console.log("value: ",`${logPercent} %`) ;
+    const day = Math.floor(1095*logPercent/100);
+    setDate(day);
+    console.log("Day:",day);
     setLists((prevLists) => [
       ...prevLists,
       {
         id: uuidv4(),
-        title: "...",
-        content: "...",
+        title: "..aaaa.",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque quidem esse? Incidunt, odit beatae?",
         createAt: "...",
       },
     ]);
   };
 
-
-  const [lists, setLists] = useState(storage);
   const listItems = lists.map((list) => (
     <li key={list.id}>
       <p>id:{list.id}</p>
       <h3 className={styles.title}>{list.title}</h3>
       <p>{list.content}</p>
-      <a onClick={()=>handleEdit(list.id)}>Edit &gt;</a>
+      <a >Edit &gt;</a>
       <a onClick={() => handleDelete(list.id)} className={styles["delete-btn"]}>
         Delete
       </a>
-      {/* <span className={styles.circle}></span> */}
-      <span  onClick={()=>handleAddBetween(list.id)} className={styles.circle}>
-          {/* &#43; */}
+      <span  className={styles.circle}>
       </span>
       <span className={styles.line}></span>
       <span className={styles.date}>{list.createAt}</span>
     </li>
   ));
-
 
   return (
     <>
@@ -65,10 +65,11 @@ function TimelineList() {
         <h3 className={styles.heading}>Timeline</h3>
         <div className={styles.container}>
           <ul>{listItems}</ul>
+          <div ref={timeLineRef} onClick={ handleChooseTimeline} className={styles.timeline}>
+            <div className={styles.circleBottom}></div>
+            <div>{date}</div>
+          </div>
         </div>
-        <button onClick={handleAdd} className={styles.btn}>
-          &#43;
-        </button>
       </div>
     </>
   );
